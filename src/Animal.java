7d2247fcb27db9635f5822751d0db357f8566ac1
GnomeShift <rainy.top@yandex.ru>
2024-11-21 16:40:07 +0300

    feat: add chance of successful hunt

--- src/Animal.java
+++ src/Animal.java
@@ -6,6 +6,15 @@ public abstract class Animal {
     private String name;
     private String icon;
     private int energy;
+    private static final Map<String, Map<String, Double>> huntChances = new HashMap<>();
+    static {
+        huntChances.put("Волк", Map.of("Лошадь", 0.10, "Олень", 0.15, "Кролик", 0.60, "Мышь", 0.80, "Коза", 0.60, "Овца", 0.70, "Кабан", 0.15, "Буйвол", 0.10, "Утка", 0.40));
+        huntChances.put("Удав", Map.of("Кролик", 0.20, "Мышь", 0.40, "Утка", 0.10));
+        huntChances.put("Лиса", Map.of("Кролик", 0.70, "Мышь", 0.90, "Утка", 0.60));
+        huntChances.put("Медведь", Map.of("Лошадь", 0.40, "Олень", 0.80, "Кролик", 0.80, "Мышь", 0.90, "Коза", 0.70, "Овца", 0.70, "Кабан", 0.50, "Буйвол", 0.20, "Утка", 0.10));
+        huntChances.put("Орел", Map.of("Кролик", 0.90, "Мышь", 0.90, "Утка", 0.80));
+        huntChances.put("Утка", Map.of("Гусеница", 0.90));
+    }
 
     public Animal(int location, String name, String icon, int energy) {
         this.location = location;
@@ -49,12 +58,21 @@ public abstract class Animal {
 
     public List<String> interact(Animal other) {
         List<String> events = new ArrayList<>();
-        if (isAlive && other.isAlive()) {
-            if (canHunt(other)) {
-                int energyPlus = hunt(other);
-                events.add(getName() + " съел(-а) " + other.getName() + " (+" + energyPlus + "⚡️)");
-                energy += energyPlus;
-                other.die();
+        if (isAlive && other.isAlive() && this != other) {
+            String hunterName = this.getName();
+            String victimName = other.getName();
+            if (huntChances.containsKey(hunterName) && huntChances.get(hunterName).containsKey(victimName) && canHunt(other)) {
+                double chance = huntChances.get(hunterName).get(victimName);
+                if (new Random().nextDouble() < chance) {
+                    int energyPlus = hunt(other);
+                    events.add(getName() + " съел(-а) " + other.getName() + " (+" + energyPlus + "⚡️)");
+                    energy += energyPlus;
+                    other.die();
+                }
+                else {
+                    events.add(getName() + " не смог съесть " + other.getName() + " (-3⚡️)");
+                    energy -= 3;
+                }
             }
         }
         return events;
